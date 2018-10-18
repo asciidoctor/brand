53839cf1349ef452e2f26bf4b90f51aa6aa45ff9
Dan Allen <dan.j.allen@gmail.com>
2018-10-18 02:13:21 -0600

    update export script to preserve transparent background

--- export-to-png.sh
+++ export-to-png.sh
@@ -5,19 +5,19 @@ OUTDIR=gen
 mkdir -p $OUTDIR
 
 # standard size
-convert logo/logo-outline-color.svg -filter Lanczos -resize 2000x $OUTDIR/asciidoctor-logo-outline-color.png
-convert logo/logo-outline-bw.svg -filter Lanczos -resize 2000x $OUTDIR/asciidoctor-logo-outline-bw.png
-convert logo/logo-fill-color.svg -filter Lanczos -resize 2000x $OUTDIR/asciidoctor-logo-fill-color.png
-convert logo/logo-fill-bw.svg -filter Lanczos -resize 2000x $OUTDIR/asciidoctor-logo-fill-bw.png
+convert -background none -filter Lanczos -resize 2000x logo/logo-outline-color.svg $OUTDIR/asciidoctor-logo-outline-color.png
+convert -background none -filter Lanczos -resize 2000x logo/logo-outline-bw.svg $OUTDIR/asciidoctor-logo-outline-bw.png
+convert -background none -filter Lanczos -resize 2000x logo/logo-fill-color.svg $OUTDIR/asciidoctor-logo-fill-color.png
+convert -background none -filter Lanczos -resize 2000x logo/logo-fill-bw.svg $OUTDIR/asciidoctor-logo-fill-bw.png
 
 # icon size
-convert logo/icon/logo-outline-color.svg -filter Lanczos -distort resize 32x $OUTDIR/asciidoctor-logo-outline-color-32.png
-convert logo/icon/logo-outline-color.svg -filter Lanczos -distort resize 16x -unsharp 0x1+.5+.25 $OUTDIR/asciidoctor-logo-outline-color-16.png
-convert logo/icon/logo-fill-color.svg -filter Lanczos -distort resize 180x $OUTDIR/asciidoctor-logo-fill-color-180.png
-convert logo/icon/logo-fill-color.svg -filter Lanczos -distort resize 64x $OUTDIR/asciidoctor-logo-fill-color-64.png
-convert logo/icon/logo-fill-color.svg -filter Lanczos -distort resize 48x $OUTDIR/asciidoctor-logo-fill-color-48.png
-convert logo/icon/logo-fill-color.svg -filter Lanczos -distort resize 32x $OUTDIR/asciidoctor-logo-fill-color-32.png
-convert logo/icon/logo-fill-color.svg -filter Lanczos -distort resize 16x -unsharp 0x1+.5+.25 $OUTDIR/asciidoctor-logo-fill-color-16.png
+convert -background none -filter Lanczos -distort resize 32x logo/icon/logo-outline-color.svg $OUTDIR/asciidoctor-logo-outline-color-32.png
+convert -background none -filter Lanczos -distort resize 16x -unsharp 0x1+.5+.25 logo/icon/logo-outline-color.svg $OUTDIR/asciidoctor-logo-outline-color-16.png
+convert -background none -filter Lanczos -distort resize 180x logo/logo-fill-color.svg $OUTDIR/asciidoctor-logo-fill-color-180.png
+convert -background none -filter Lanczos -distort resize 64x logo/icon/logo-fill-color.svg $OUTDIR/asciidoctor-logo-fill-color-64.png
+convert -background none -filter Lanczos -distort resize 48x logo/icon/logo-fill-color.svg $OUTDIR/asciidoctor-logo-fill-color-48.png
+convert -background none -filter Lanczos -distort resize 32x logo/icon/logo-fill-color.svg $OUTDIR/asciidoctor-logo-fill-color-32.png
+convert -background none -filter Lanczos -distort resize 16x -unsharp 0x1+.5+.25 logo/icon/logo-fill-color.svg $OUTDIR/asciidoctor-logo-fill-color-16.png
 
 # favicon
 icotool -c -o $OUTDIR/favicon.ico \
